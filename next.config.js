6a0ed797d5f6ed1556c634122fa7fd4dacfba904
Melake12 <wubshetmelake@gmail.com>
2024-01-12 19:01:25 +0300

    feat: image remove button is added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '1337',
+                pathname: '/uploads/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'uploadthing.com',
+
+            },
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+
+            }
+        ],
+    },
+}
 
 module.exports = nextConfig
