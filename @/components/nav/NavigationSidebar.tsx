7677fbf6200219242f79b8d5e6d8e2e0af758855
Melake12 <wubshetmelake@gmail.com>
2024-01-19 23:38:19 +0300

    feat: channels list added

--- @/components/nav/NavigationSidebar.tsx
+++ @/components/nav/NavigationSidebar.tsx
@@ -2,6 +2,9 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import NavigationAction from "./NavigationAction";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { NavigationItem } from "./navigation-item";
 
 const NavigationSidebar = async () => {
     const profile = await currentProfile();
@@ -9,7 +12,7 @@ const NavigationSidebar = async () => {
         redirect('/');
     }
 
-    const servers = db.server.findMany({
+    const servers = await db.server.findMany({
         where: {
             members: {
                 some: {
@@ -22,6 +25,18 @@ const NavigationSidebar = async () => {
     return ( 
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
             <NavigationAction />
+
+            <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"/>
+
+            <ScrollArea className="flex-1 w-full">
+                {
+                    servers.map((server) => (
+                        <div key={server.id} className="mb-4">
+                            <NavigationItem id={server.id} name={server.name} imageUrl={server.imageUrl}/>
+                        </div>
+                    ))
+                }
+            </ScrollArea>
         </div>
     );
 }
