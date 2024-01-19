7c0503cb57e50829e17bffd38bfcffe9a064a97b
Melake12 <wubshetmelake@gmail.com>
2024-01-20 00:00:02 +0300

    feat: modal provider is added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from '@/lib/utils'
+import { ModalProvider } from '@/components/providers/modal-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -29,6 +30,8 @@ export default function RootLayout({
             enableSystem = {false}
             storageKey='discord-clone-theme'
           >
+            <ModalProvider />
+            
             {children}
           </ThemeProvider>
         </body>
